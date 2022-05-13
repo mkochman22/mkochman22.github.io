5233a0d2a9bed06393f462252bd9ba15916917b7
mkochman22 <95151862+mkochman22@users.noreply.github.com>
2022-05-12 22:58:40 -0400

    Add files via upload

---
+++ Quizzes/Quiz1/questions.js
@@ -0,0 +1,69 @@
+const questions = [
+  {
+    "question": "Where are you going?",
+    "answer1": "Tokyo",
+    "answer1Total": "1",
+    "answer2": "New York",
+    "answer2Total": "2",
+    "answer3": "Hawaii",
+    "answer3Total": "3"
+  },
+    {
+    "question": "Who are you going with?",
+    "answer1": "My Family",
+    "answer1Total": "1",
+    "answer2": "Myself",
+    "answer2Total": "2",
+    "answer3": "Friends",
+    "answer3Total": "3"
+  },
+  {
+    "question": "How are you getting there?",
+    "answer1": "Cruise",
+    "answer1Total": "1",
+    "answer2": "Train",
+    "answer2Total": "2",
+    "answer3": "Private Jet",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Where are you staying?",
+    "answer1": "Mansion",
+    "answer1Total": "1",
+    "answer2": "Hotel",
+    "answer2Total": "3",
+    "answer3": "AirBnb",
+    "answer3Total": "2"
+  },
+  {
+    "question": "Pick an activity",
+    "answer1": "Escape Room",
+    "answer1Total": "3",
+    "answer2": "Hiking",
+    "answer2Total": "2",
+    "answer3":
+      "Skydiving",
+    "answer3Total": "1"
+  },
+  {
+    "question": "Pick another activity",
+    "answer1": "Wine tour",
+    "answer1Total": "1",
+    "answer2": "Cooking class",
+    "answer2Total": "2",
+    "answer3": "Visit historical places",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Pick a souvenir",
+    "answer1":
+      "Hoodie",
+    "answer1Total": "3",
+    "answer2": "Mug/Waterbottle",
+    "answer2Total": "2",
+    "answer3": "T-Shirt",
+    "answer3Total": "1"
+  }
+]
